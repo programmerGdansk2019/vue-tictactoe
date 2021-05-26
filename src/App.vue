3fec1cc27cf1ccc36c70277e750e38958d6eeebb
ProgrammerGdansk2019 <kuba4live@gmail.com>
2021-05-26 18:57:18 +0200

    Branch1: First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="game" :class="{no_events: counter === 6}">
+    <div class="board">
+      <div class="row" v-for="(row, i) in board" :key="row">
+        <div class="cell" :class="[cell]" @click="setColor(i, j)" v-for="(cell, j) in row" :key="cell">
+          </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    data(){
+      return{
+        board: [
+          ["", "", ""],
+          ["", "", ""],
+        ],
+        colors: [
+          ["red", "blue", "green"],
+          ["blue", "red", "green"],
+        ],
+        counter: 0,
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+      }
+
+    },
+    methods:{
+      setColor(i, j){
+        this.board[i][j] = this.colors[i][j];
+
+        for (let i = 0; i < 2; i++) {
+          for (let j = 0; j < 3; j++) {
+            if (this.board[i][j] !== "") {
+              this.counter++;
+            }
+          }
+
+        }
+        if (this.counter === 6) {
+          console.log("Koniec Gry");
+        }
+        console.log(this.counter);
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .red{
+    background-color: red;
+  }
+  .green{
+    background-color: green;
+  }
+  .blue{
+    background-color: blue;
+  }
+  .game{
+    display: flex;
+    justify-content: center;
+  }
+  .cell{
+    width: 200px;
+    height: 200px;
+    border: 1px solid black;
+    cursor: pointer;
+  }
+  .board{
+    width: 600px;
+    display: flex;
+    flex-direction: column;
+  }
+  .row{
+    width: 600px;
+    display: flex;
+  }
+  .no_events{
+    pointer-events: none;
+  }
 </style>
+
+
